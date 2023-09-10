c564758ee20d7f26e38875928bc2f73eb089a93e
nasirmondol <nasirahmed8202@gmail.com>
2023-09-10 08:40:47 +0600

    countries data load and display ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './components/Country/Countries'
+
+function App() {
+
+  return (
+    <>
+      <h2>React Countries of the  world</h2>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SingleCountry/Country.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './country.css'
+
+const Country = ({ country }) => {
+    console.log(country);
+    const { name, flags } = country;
+    return (
+        <div className='country'>
+            <h3>{name?.common}</h3>
+            <img src={flags?.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
